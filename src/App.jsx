e1a196f8e8a317cf1951a77a1cf6dd97056e8185
Adriano <adrianorsavi@gmail.com>
2023-05-21 21:45:18 -0300

    Alteração do arquivo App.jsx e mudanças no código

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,66 @@
 import "./App.css";
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h2>Lista de Compras:</h2>
-      </header>
-      <div className="lista-compras-container">
-        <ul className="lista-items">
-          <li>Queijo</li>
-          <li>Leite</li>
-          <li>Pão</li>
-        </ul>
-      </div>
-      <form className="form-add-item" action="#" method="post">
-        <fieldset>
-          <div className="form-group mb-3">
-            <label htmlFor="item">Adicionar Novo Item na Lista:</label>
-            <input type="text" className="form-control" id="item" />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Adicionar
-          </button>
-        </fieldset>
-      </form>
-    </div>
-  );
+function ListaCompras({ itens }) {
+    return (
+        <div className="lista-compras-container">
+            <ul className="lista-items">
+                {itens.map((item) => (<li>{item}</li>))}
+            </ul>
+        </div>
+    );
 }
 
-export default App;
+function FormularioAdicionarItens({ addItem }) {
+    const [item, setItem] = useState([]);
+
+    const enviaForm = (event) => {
+        event.preventDefault();
+
+        if (item === '') {
+            alert('Informe o nome!');
+            return;
+        }
+
+        addItem(item);
+        setItem('');
+    };
+
+    return (
+        <form action="#" method="post" onSubmit={enviaForm}>
+            <fieldset>
+                <div className="form-group mb-3">
+                    <label htmlFor="item">Adicionar Novo Item na Lista:</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="item"
+                        value={item}
+                        onChange={(e) => setItem(e.target.value)}
+                    />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                    Adicionar
+                </button>
+            </fieldset>
+        </form>
+    );
+}
+
+export default function App() {
+    const [itens, setItens] = useState([]);
+
+    const addItem = (item) => {
+        setItens([...itens, item]);
+    };
+
+    return (
+        <div className="App">
+            <header>
+                <h2>Lista de Compras:</h2>
+            </header>
+            <ListaCompras itens={itens} />
+            <FormularioAdicionarItens addItem={addItem} />
+        </div>
+    );
+}
